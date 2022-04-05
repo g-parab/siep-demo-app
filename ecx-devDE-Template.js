3d63891c17ffe323db5b7b64158b65813d5b6b2f
snehal1510 <rsnehal76@gmail.com>
2022-04-05 18:51:44 +0530

    Update ecx-devDE-Template.js

--- ecx-devDE-Template.js
+++ ecx-devDE-Template.js
@@ -1,16 +1,16 @@
- // *** snippet starts here ***
+  // *** snippet starts here ***
   // ***************************
  
   // Example (please overwrite):
   var initESW = function (gslbBaseURL) {
     embedded_svc.settings.displayHelpButton = true; //Or false
-    embedded_svc.settings.language = ''; //For example, enter 'en' or 'en-US'
-   // embedded_svc.settings.targetElement =  chatbotScriptsParameters.chatbotParent;
+    embedded_svc.settings.language = 'de'; //For example, enter 'en' or 'en-US'
+    //embedded_svc.settings.targetElement =  chatbotScriptsParameters.chatbotParent;
  
-    //embedded_svc.settings.defaultMinimizedText = '...'; //(Defaults to Chat with an Expert)
+    embedded_svc.settings.defaultMinimizedText = 'Chatten Sie mit einem Experten'; //(Defaults to Chat with an Expert)
     //embedded_svc.settings.disabledMinimizedText = '...'; //(Defaults to Agent Offline)
  
-    //embedded_svc.settings.loadingText = ''; //(Defaults to Loading)
+    embedded_svc.settings.loadingText = 'Wird geladen'; //(Defaults to Loading)
     //embedded_svc.settings.storageDomain = 'yourdomain.com'; //(Sets the domain for your deployment so that visitors can navigate subdomains during a chat session)
  
     // Settings for Chat
@@ -26,19 +26,19 @@
     embedded_svc.settings.entryFeature = 'LiveAgent';
  
     embedded_svc.init(
-      'https://sieportal--dev.my.salesforce.com',
-	  'https://dev-sieportal-dev.cs107.force.com/heim',
+      'https://sieportal--stage.my.salesforce.com',
+	  'https://stage-sieportal.cs102.force.com/start',
       gslbBaseURL,
-      '00D1q0000008hJr',
-      'SP_Chat_Agents_English',
-      {
-			baseLiveAgentContentURL: 'https://c.la1-c1cs-cdg.salesforceliveagent.com/content',
-			deploymentId: '5721q00000000mn',
-			buttonId: '5731q00000002wn',
-			baseLiveAgentURL: 'https://d.la1-c1cs-cdg.salesforceliveagent.com/chat',
-			eswLiveAgentDevName: 'EmbeddedServiceLiveAgent_Parent04I1q0000004EOyEAM_17bab652541',
+      '00D1j0000008eIR',
+      'SP_Chat_Agents_German',
+		{
+			baseLiveAgentContentURL: 'https://c.la1-c1cs-fra.salesforceliveagent.com/content',
+			deploymentId: '5721j0000008QRi',
+			buttonId: '5731j0000008SHw',
+			baseLiveAgentURL: 'https://d.la1-c1cs-fra.salesforceliveagent.com/chat',
+			eswLiveAgentDevName: 'EmbeddedServiceLiveAgent_Parent04I1j00000001JFEAY_17e496d098b',
 			isOfflineSupportEnabled: false
-      }
+		}
     );
   };
  
@@ -51,19 +51,19 @@
           label: 'Token',
           displayToAgent: false,
           transcriptFields: ['SP_Authentication_Token__c'],
-          value: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IkI0NjhEODMyOEY4NjdGNTgyRjNENjBGN0IzOUIwREU0MjYyQzZEOUZSUzI1NiIsInR5cCI6ImF0K2p3dCIsIng1dCI6InRHallNby1HZjFndlBXRDNzNXNONUNZc2JaOCJ9.eyJuYmYiOjE2NDgwMzMzOTgsImV4cCI6MTY0ODAzNjk5OCwiaXNzIjoiaHR0cHM6Ly9hdXRoLnNpZXBvcnRhbGludGVncmF0aW9uLnNpZW1lbnMuY29tIiwiYXVkIjpbIkluZHVzdHJ5IE1hbGwiLCJjb2NrcGl0IiwiU2llbWVucyBJbmR1c3RyeSBPbmxpbmUgU3VwcG9ydCIsInJlY2VudGx5dmlld2VkIiwibWFya2V0aW5nIiwicXVpY2tsaW5rcyIsInVzZXJwcmVmZXJlbmNlcyIsIm1haW5uYXZpZ2F0aW9uIiwicmVjb21tZW5kYXRpb24iXSwiY2xpZW50X2lkIjoiU2llbWVucy5TaWVQb3J0YWwuVUkiLCJzdWIiOiJDQkIwMDUyNDhERjlFMkZBQjA4QkU2ODZFN0JERTc4QTJERkZEQjRBQUNENjEzQjM2NzQ5NkIxMzQ3RkFGNTdDIiwiYXV0aF90aW1lIjoxNjQ4MDMzMzk2LCJpZHAiOiJTYW1sMiIsInVybjpvaWQ6c3NvOmdpdmVubmFtZSI6IkNoYXQiLCJ1cm46b2lkOnNzbzpzaW9zcHQiOiJ0cnVlIiwidXJuOm9pZDpzc286bWFpbCI6InNpZXBvcnRhbC10ZXN0dGVhbS5pdEBzaWVtZW5zLmNvbSIsInVybjpvaWQ6c3NvOnNzb3VpZCI6IjE5NUE5RjA4LTgxMEUtNDUyNi05MkM1LTNDODg1MEU3NTBFMSIsInVybjpvaWQ6c3NvOmNyZWQiOiJTaWVwX0IwX0NoYXRfMDIiLCJ1cm46b2lkOnNzbzpmYW1pbHluYW1lIjoiQm90MDIiLCJyb2xlIjoidXNlciIsImluaXRpYWxzIjoiQ0IiLCJsYXN0LWxvZ2luIjoiMjAyMi0wMy0yM1QxMTowMzoxOCArMDA6MDAiLCJqdGkiOiI4RDU5MEIyQTdFMjkxMEM5RDVCOTExRkIzNDM4NURDRCIsInNpZCI6IkQzRkRFMENFQUU1RTRFNTkwMUM2MkZFOEE3QTczQUFEIiwiaWF0IjoxNjQ4MDMzMzk4LCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwibGFzdC1sb2dpbiIsImluaXRpYWxzIiwidXJuOm9pZDpzc286ZmFtaWx5bmFtZSIsInVybjpvaWQ6c3NvOmdpdmVubmFtZSIsInVzZXJwcmVmZXJlbmNlcyIsInF1aWNrbGlua3MiLCJjb2NrcGl0IiwibWFya2V0aW5nIiwicmVjZW50bHl2aWV3ZWQiLCJtYWlubmF2aWdhdGlvbiIsInJlY29tbWVuZGF0aW9uIiwibWFsbCIsInNpb3MiXSwiYW1yIjpbImV4dGVybmFsIl19.qlSGV8RgkkZS4t96NWmTv_HiIzv4IC4j9I7g1Cs_cPiwiNAmjZ7f-y4P-LwrzgryH6bS3Y4_GcKSxX_KK3L9c6TDDA8LElHXCYF-bD6yJyvOOeH-q5DRW4lGjAmMmDNogdnCqOBx1drUPQAt_kWcn5adyRXpvN5aEyQHjdBsblaiKVO3myTCys9KKrnCOXaE7V0iVxQHevfr1Xs0qYtuQYhhwIVR63wKt6rNq8G1gJOIA-ROBGpKCRXkHa3iW68vYl53A25TyRuR4ck8cOHtY7y8wTNbIco1vbrT_gEShqxeMIIJhbxpV3pm4-dFzqGJ9uwUOVKZzx_zFtgDEApzoIJjDebnqD4sY-43TnvRCPPIARaaWWka2zCVMApoH6trctcRDFAl4A6fUEbJ0uwIGY2LkuqOGIIjIBVvzqlj5kk6f6jKTr3iPdLGR5WYGV0yW9-H_vGzOayd2qKEtnUq4ZMQIuJJHb_ITRs9-bLG_JWqiW9cLbr488fkh3e4TgHju9-vgGGZxtvYhUied-8H-R8JCZhLKjMM82R4ERhXKcilX01RhI72dqjQq6AQH2vu-dvwYuJMsEDwt6zxTHg3fJX_KCSogEz2czspb9Pe48vXLG0Dp4cgDQukFctW5F0IXDA6HyCtC6cZhF2we9w2Ikex3n_V5fDquSEwd_dOWIo'//chatbotScriptsParameters.token
+          value: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IkI0NjhEODMyOEY4NjdGNTgyRjNENjBGN0IzOUIwREU0MjYyQzZEOUZSUzI1NiIsInR5cCI6ImF0K2p3dCIsIng1dCI6InRHallNby1HZjFndlBXRDNzNXNONUNZc2JaOCJ9.eyJuYmYiOjE2NDkxNTg5OTcsImV4cCI6MTY0OTE2MjU5NywiaXNzIjoiaHR0cHM6Ly9hdXRoLnNpZXBvcnRhbGRldi5zaWVtZW5zLmNvbSIsImF1ZCI6WyJJbmR1c3RyeSBNYWxsIiwiY29ja3BpdCIsIlNpZW1lbnMgSW5kdXN0cnkgT25saW5lIFN1cHBvcnQiLCJyZWNlbnRseXZpZXdlZCIsIm1hcmtldGluZyIsInF1aWNrbGlua3MiLCJ1c2VycHJlZmVyZW5jZXMiLCJtYWlubmF2aWdhdGlvbiIsInJlY29tbWVuZGF0aW9uIl0sImNsaWVudF9pZCI6IlNpZW1lbnMuU2llUG9ydGFsLlVJIiwic3ViIjoiQjU5NUY4RTJGQkRDOEMzRjYzRkQzOTY4NzdCMDNFRDVERDc3ODQzOENDMDIzMjRGNDFENkRCNDlEMDAyRDQwQSIsImF1dGhfdGltZSI6MTY0OTE1ODk5NCwiaWRwIjoiU2FtbDIiLCJ1cm46b2lkOnNzbzpmYW1pbHluYW1lIjoiQm90MDIiLCJ1cm46b2lkOnNzbzpjcmVkIjoiU2llcF9CMF9DaGF0XzAyIiwidXJuOm9pZDpzc286c3NvdWlkIjoiMTk1QTlGMDgtODEwRS00NTI2LTkyQzUtM0M4ODUwRTc1MEUxIiwidXJuOm9pZDpzc286bWFpbCI6InNpZXBvcnRhbC10ZXN0dGVhbS5pdEBzaWVtZW5zLmNvbSIsInVybjpvaWQ6c3NvOnNpb3NwdCI6InRydWUiLCJ1cm46b2lkOnNzbzpnaXZlbm5hbWUiOiJDaGF0Iiwicm9sZSI6InVzZXIiLCJpbml0aWFscyI6IkNCIiwibGFzdC1sb2dpbiI6IjIwMjItMDQtMDVUMTE6NDM6MTcgKzAwOjAwIiwianRpIjoiOTVBMTU5MEEyQUM2MjQ5MTQyQTBBMDVEN0ZGNTU4QjYiLCJzaWQiOiI1NjkwQjhCRkUxNEY2OEIwRDhDMjBGOTk1RUMxNUI5MiIsImlhdCI6MTY0OTE1ODk5Nywic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsImxhc3QtbG9naW4iLCJpbml0aWFscyIsInVybjpvaWQ6c3NvOmZhbWlseW5hbWUiLCJ1cm46b2lkOnNzbzpnaXZlbm5hbWUiLCJ1c2VycHJlZmVyZW5jZXMiLCJxdWlja2xpbmtzIiwiY29ja3BpdCIsIm1hcmtldGluZyIsInJlY2VudGx5dmlld2VkIiwibWFpbm5hdmlnYXRpb24iLCJyZWNvbW1lbmRhdGlvbiIsIm1hbGwiLCJzaW9zIl0sImFtciI6WyJleHRlcm5hbCJdfQ.j74PV8PqRXXtgamPCvWxu02JFQY_t-Xkm2yQU-iZ-cOG4kUxYqmAdIVwQ2SQti4wN3aJ2q1_ruHnMmyf_xgoOYeFiM2icnEj0BUZjzOAW4LxYnDacnKyum75bderoyFUCDUU54MELUGhESVjUZ_2F9KM0atT9EZmuquE1C9Ps64EZXAmNReKVADF1UGKHEd8EPQ4vzhUSwNilIGWqJWXfLQ0wKYQ0Pkc7wGnM1rl_3MRkAMO5uTU6WdcQCTgx1-axa_bG9KPZHPn2B2IQCaGfc2pXakehIAqQVMKnV9RkfkD50LoALhCYw65qAKb-qL6ip-5iywAI8noZ5TZFYdle-eaZVDauOpLpGy_6-C3yYehp3ORmRbqa4sWNj9kD13Chnz_c6D3yhTFdR_dYIjzKG-B6nQmEq-GoLED8YUzvN6Hp4TvTGVNhVMkM0_djLgWGbeD6slCt7h3g7C1Z3xfAymqYvhK4QUhHiCOk1TxUPvLT92bvdEhj1EsLpjKPNPZnnDEUs33zhvmewftGhZcb4HV0LsFB3jLBVzVktIw2uWugd33f0cyyAHIY2fw0Jffl--Zb2AB1urknLun9ONs6ewDL3SH1FRIXrdo5ZsBjcrGaJvgTcXG_ZWy20Eid0EJJbk0FGWfWcllAjIv73qgQyaEWHBOzSuqS9SITH-FMb0'//chatbotScriptsParameters.token
         },
         {
           label: 'Country',
           displayToAgent: true,
           transcriptFields: ['SP_Country__c'],
-          value: 'GB'//chatbotScriptsParameters.country
+          value: 'DE'//chatbotScriptsParameters.country
         },
         {
           label: 'Language',
           displayToAgent: true,
           transcriptFields: ['SP_Language__c'],
-          value: 'en'//chatbotScriptsParameters.language
+          value: 'de'//chatbotScriptsParameters.language
         });
         event.detail.callback();
     },
@@ -72,7 +72,7 @@
  
   if (!window.embedded_svc) {
     var s = document.createElement('script');
-    s.setAttribute('src', 'https://sieportal--dev.my.salesforce.com/embeddedservice/5.0/esw.min.js');
+    s.setAttribute('src', 'https://sieportal--stage.my.salesforce.com/embeddedservice/5.0/esw.min.js');
     s.onload = function () {
       initESW(null);
     };
